8b21a0918198735e40099d343e9101fd827a47a8
Baldwin <palenciaserranobaldwin@gmail.com>
2026-03-06 21:48:17 -0500

    Nuevo PIDO2.1

--- proyectopido-github-io/src/App.js
+++ proyectopido-github-io/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Aqui va lo del login sgsdf
+          Aqui va lo u
         </p>
         <a
           className="App-link"
